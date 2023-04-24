4aa33d6083db269311f6317211b61077b983cc74
anna.lomenova <anna.lomenova@ikasle.egibide.org>
2023-04-24 17:48:18 +0200

    falta calendario y spinners

--- app/src/main/java/com/example/proyecto_ong/RegistrarUsuarioFragment.kt
+++ app/src/main/java/com/example/proyecto_ong/RegistrarUsuarioFragment.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import android.widget.Toast
 import androidx.core.widget.doOnTextChanged
 import androidx.navigation.fragment.findNavController
 import com.example.proyecto_ong.databinding.FragmentFirstBinding
@@ -31,10 +33,36 @@ class RegistrarUsuarioFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         binding.bRegistrar.setOnClickListener {
-            findNavController().navigate(R.id.action_registrarUsuarioFragment_to_SecondFragment)
+            if(validarDatosUsuario()){
+                findNavController().navigate(R.id.action_FirstFragment_to_SecondFragment)
+            }
         }
 
 
+    }
+
+    private fun validarDatosUsuario(): Boolean {
+        if(binding.etNombre.text.toString().isEmpty() || binding.etNombre.text.toString().length < 3){
+            showError(binding.etNombre, "El usuario debe que tener minimo 3 carácteres.")
+            return false
+        }else if (binding.etContrasena1.text.toString().isEmpty()){
+            showError(binding.etContrasena1, "Campo contraseña es obligatorio.")
+            return false
+        }else if (binding.etContrasena2.text.toString().isEmpty()) {
+            showError(binding.etContrasena2, "Campo contraseña es obligatorio.")
+            return false
+        }else if(binding.etContrasena1.text.toString() != binding.etContrasena2.text.toString()){
+            showError(binding.etContrasena2, "Contreseñas no son iguales.")
+            return false
+        }
+        return true
+
+        //FALTA BUSCAR SI USUARIO YA EXISTE
+    }
+
+    private fun showError(input: EditText, s: String) {
+        input.setError(s)
+        input.requestFocus()
     }
 
     override fun onDestroyView() {
